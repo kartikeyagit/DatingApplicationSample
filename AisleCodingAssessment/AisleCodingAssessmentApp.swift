1bb8808a2cffb661881a27db2bf7fb26982589d1
kartikeyagit <kartikeya85@gmail.com>
2024-11-07 09:41:42 +0530

    Initial Commit

---
+++ AisleCodingAssessment/AisleCodingAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AisleCodingAssessmentApp.swift
+//  AisleCodingAssessment
+//
+//  Created by Kartikeya Singh on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AisleCodingAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
